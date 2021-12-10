a73010cf9c0cf8eb03f63a2e9ac6b9e4e3b4d03a
woongcheol <1994_100@naver.com>
2021-12-10 12:21:56 +0900

    add mypage

--- heat/accountapp/urls.py
+++ heat/accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accountapp.views import AccountCreateview, hello_world
+from accountapp.views import AccountCreateView, hello_world, AccountDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "accountapp"
@@ -8,5 +8,6 @@ urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('create/', AccountCreateview.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
--- heat/accountapp/views.py
+++ heat/accountapp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.urls.base import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from accountapp.models import HelloWorld
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
@@ -25,8 +26,13 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
-class AccountCreateview(CreateView):
+class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
